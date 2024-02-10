76781a8d13c135c812ade17b6f340a0deea4847e
Charlie Lee <charlie.lee.trainer@gmail.com>
2024-02-10 18:39:25 +0000

    Angular Project

---
+++ AngTheatre/src/app/tragedy/tragedy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tragedy',
+  templateUrl: './tragedy.component.html',
+  styleUrl: './tragedy.component.css'
+})
+export class TragedyComponent {
+
+}
---
+++ AngularTheatre/src/app/tragedy/tragedy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tragedy',
+  standalone: true,
+  imports: [],
+  templateUrl: './tragedy.component.html',
+  styleUrl: './tragedy.component.css'
+})
+export class TragedyComponent {
+
+}
