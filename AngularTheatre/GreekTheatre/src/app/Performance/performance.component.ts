8061622ea36f67f8a404d837882d5f624e927b6a
Charlie Lee <charles.lee@pureretirement.co.uk>
2024-02-21 15:39:56 +0000

    Factored out hard-coded text to typescript file for more modularity.

--- AngularTheatre/GreekTheatre/src/app/Performance/performance.component.ts
+++ AngularTheatre/GreekTheatre/src/app/Performance/performance.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { performanceP1, performanceP2, performanceP3,  performanceP4, performanceP5, performanceP6, performanceP7 } from './performancetext';
+import { NgClass, NgFor } from '@angular/common';
+
+// This section wants to be factored out to a seperate file, so that changes on logic and text don't require branches on the same file.
+
 
 @Component({
   selector: 'app-performance',
@@ -6,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrl: '../app.component.css'
 })
 export class PerformanceComponent {
-  title = 'Performance'
+
+  // Page title
+  title = 'Performance';
+  // Main paragraphs
+  p1=performanceP1;
+  p2=performanceP2;
+  p3=performanceP3;
+  p4=performanceP4;
+  p5=performanceP5;
+  p6=performanceP6;
+  p7=performanceP7;
+  // Bootstrap + custom CSS classes
+  right_paragraph = "col-12 col-xs-12 col-sm-6 col-md-6 col-lg-8 right-info"
+  left_paragraph = "col-12 col-xs-12 col-sm-6 col-md-6 col-lg-8 left-info"
+  // Implement
+
+
 }
