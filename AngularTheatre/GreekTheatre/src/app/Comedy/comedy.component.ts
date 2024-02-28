eaa7eb5277c29421f84789405001762cf097ce9a
Charlie Lee <charles.lee@pureretirement.co.uk>
2024-02-28 15:03:36 +0000

    refactor: remove repetition by factoring out to ngClass and ngFor loops.

--- AngularTheatre/GreekTheatre/src/app/Comedy/comedy.component.ts
+++ AngularTheatre/GreekTheatre/src/app/Comedy/comedy.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { t1, t2, t3, p1s, p2s, p3s, sections } from './comedytext'
 import { AllCapsPipe } from '../Pipes/all-caps.pipe';
 
 @Component({
@@ -6,8 +9,19 @@ import { AllCapsPipe } from '../Pipes/all-caps.pipe';
   selector: 'app-comedy',
   templateUrl: './comedy.component.html',
   styleUrl: '../app.component.css',
-  imports: [AllCapsPipe]
+  imports: [
+    CommonModule,
+    NgClass, 
+    NgFor, 
+    NgIf,
+    AllCapsPipe
+  ]
 })
 export class ComedyComponent {
   title = 'Comedy';
+  body: sections[] = [
+    {title: t1, img: '../assets/images/Aristophanes.jpg', alttext: 'Aristophanes, the playwright', paragraphs: p1s},
+    {title: t2, img: '../assets/images/theatremask.jpg', alttext: 'A Classical theatre mask', paragraphs: p2s},
+    {title: t3, img: '../assets/images/Menander.jpg', alttext: 'Menander, the playwright', paragraphs: p3s},
+  ]
 }
